3dc96ff4bb8949c891fc84a1c7e6ff4180407a5f
abdelrahmansalahkhamis <abdulrahmansalah86@gmail.com>
2021-06-18 02:38:27 +0200

    first commit

---
+++ lib/question.dart
@@ -0,0 +1,12 @@
+class Question{
+  Question({required this.q, required this.a});
+  String q;
+  bool a;
+
+  // Question({String q, bool a}){
+  //   questionText = q;
+  //   questionAnswer = a;
+  // }
+
+
+}
